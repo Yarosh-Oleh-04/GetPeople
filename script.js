77f9f3080bd89e648bf7fe2e985db4f36d40ce46
Yarosh-Oleh-04 <yarosholeh04@gmail.com>
2018-12-27 20:06:09 +0200

    hisgi

--- script.js
+++ script.js
@@ -5,15 +5,14 @@ xml.send()
 xml.onreadystatechange = function() {
     if (xml.readyState == 4 && xml.status == 200){
         let data = JSON.parse(this.responseText)
-        console.log(data)
         for (i = 0; i < data.people.length; i++){
             var div = document.createElement('div')
             div.className = 'people'
             document.body.appendChild(div)
            div.innerHTML += '<img src="https://api.hebron-academy.com/itacademyhebron/api/media/' + data.people[i].image + '">'
-           div.innerHTML += data.people[i].name
-           div.innerHTML += data.people[i].position
-           div.innerHTML += '<a href="' + data.people[i].facebook + '">' + 'Facebook'
+           div.innerHTML += '<p>' + data.people[i].name
+           div.innerHTML += '<p>' + data.people[i].position
+           div.innerHTML += '<a target="blank" href="' + data.people[i].facebook + '">' + 'Facebook'
         }
     }
 }
